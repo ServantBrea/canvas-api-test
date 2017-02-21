df339ca07e8d884850939ab152ad3cb8591f7efa
ServantBrea <516916849@qq.com>
2017-02-21 18:06:02 +0800

    Finish Homework 2

--- out/main.js
+++ out/main.js
@@ -3,6 +3,82 @@ var __extends = (this && this.__extends) || function (d, b) {
     function __() { this.constructor = d; }
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+window.onload = function () {
+    var canvas = document.getElementById("myCanvas");
+    var canvas2D = canvas.getContext("2d");
+    var background = new DisplayObjectContainer();
+    var container = new DisplayObjectContainer();
+    container.x = 10;
+    container.y = 20;
+    container.alpha = 0.5;
+    var text1 = new TextField();
+    text1.x = 0;
+    text1.y = 0;
+    text1.alpha = 0.8;
+    text1.color = "#FF0000";
+    text1.fontSize = 30;
+    text1.font = "Arial";
+    text1.text = "I lose my game of life!";
+    var text2 = new TextField();
+    text2.x = 0;
+    text2.y = 1;
+    text2.alpha = 1;
+    text2.color = "#0000FF";
+    text2.fontSize = 30;
+    text2.font = "Arial";
+    text2.text = "落命.....";
+    var bitmap = new Bitmap();
+    bitmap.x = 0;
+    bitmap.y = 0;
+    bitmap.alpha = 0.8;
+    bitmap.scaleX = 1;
+    bitmap.scaleY = 1;
+    bitmap.src = "codmw.png";
+    background.addChild(container);
+    container.addChild(text2);
+    background.addChild(text1);
+    background.addChild(bitmap);
+    background.draw(canvas2D);
+    setInterval(function () {
+        canvas2D.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(canvas2D);
+    }, 30);
+};
+var DisplayObject = (function () {
+    function DisplayObject() {
+        this.x = 0;
+        this.y = 0;
+        this.scaleX = 1;
+        this.scaleY = 1;
+        this.rotation = 0;
+        this.alpha = 1;
+        this.globalAlpha = 1;
+        this.matrix = new math.Matrix();
+        this.globalMatrix = new math.Matrix();
+        this.parent = null;
+    }
+    //模版draw方法
+    DisplayObject.prototype.draw = function (canvas) {
+        this.matrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation);
+        if (this.parent) {
+            this.globalAlpha =
+                this.parent.globalAlpha * this.alpha;
+            this.globalMatrix =
+                math.matrixAppendMatrix(this.matrix, this.globalMatrix);
+        }
+        else {
+            this.globalAlpha = this.alpha;
+            this.globalMatrix = this.matrix;
+        }
+        canvas.globalAlpha = this.globalAlpha;
+        canvas.setTransform(1, 0, 0, 1, 0, 0);
+        canvas.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
+        this.render(canvas);
+    };
+    //子类重载渲染
+    DisplayObject.prototype.render = function (canvas) { };
+    return DisplayObject;
+}());
 var DisplayObjectContainer = (function (_super) {
     __extends(DisplayObjectContainer, _super);
     function DisplayObjectContainer() {
@@ -35,40 +111,6 @@ var DisplayObjectContainer = (function (_super) {
     };
     return DisplayObjectContainer;
 }(DisplayObject));
-var DisplayObject = (function () {
-    function DisplayObject() {
-        this.x = 0;
-        this.y = 0;
-        this.scaleX = 1;
-        this.scaleY = 1;
-        this.rotation = 0;
-        this.alpha = 1;
-        this.globalAlpha = 1;
-        this.matrix = new math.Matrix();
-        this.globalMatrix = new math.Matrix();
-        this.parent = null;
-    }
-    //模版draw方法
-    DisplayObject.prototype.draw = function (canvas) {
-        this.matrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation);
-        if (this.parent) {
-            this.globalAlpha =
-                this.parent.globalAlpha * this.alpha;
-            this.globalMatrix =
-                math.matrixAppendMatrix(this.matrix, this.globalMatrix);
-        }
-        else {
-            this.globalAlpha = this.alpha;
-            this.globalMatrix = this.matrix;
-        }
-        canvas.globalAlpha = this.globalAlpha;
-        canvas.setTransform(this.globalMatrix.a, this.globalMatrix.b, this.globalMatrix.c, this.globalMatrix.d, this.globalMatrix.tx, this.globalMatrix.ty);
-        this.render(canvas);
-    };
-    //子类重载渲染
-    DisplayObject.prototype.render = function (canvas) { };
-    return DisplayObject;
-}());
 var Bitmap = (function (_super) {
     __extends(Bitmap, _super);
     function Bitmap() {
@@ -117,45 +159,4 @@ var TextField = (function (_super) {
     };
     return TextField;
 }(DisplayObject));
-window.onload = function () {
-    var canvas = document.getElementById("myCanvas");
-    var canvas2D = canvas.getContext("2d");
-    var background = new DisplayObjectContainer();
-    var container = new DisplayObjectContainer();
-    container.x = 100;
-    container.y = 50;
-    container.alpha = 0.5;
-    var text1 = new TextField();
-    text1.x = 0;
-    text1.y = 0;
-    text1.alpha = 0.8;
-    text1.color = "#FF0000";
-    text1.fontSize = 30;
-    text1.font = "Arial";
-    text1.text = "I lose my game of life!";
-    var text2 = new TextField();
-    text2.x = 100;
-    text2.y = 100;
-    text2.alpha = 1;
-    text2.color = "#0000FF";
-    text2.fontSize = 30;
-    text2.font = "Arial";
-    text2.text = "落命.....";
-    var bitmap = new Bitmap();
-    bitmap.x = 0;
-    bitmap.y = 0;
-    bitmap.alpha = 0.8;
-    bitmap.scaleX = 1;
-    bitmap.scaleY = 1;
-    bitmap.src = "codmw.png";
-    background.addChild(container);
-    container.addChild(text1);
-    container.addChild(text2);
-    container.addChild(bitmap);
-    background.draw(canvas2D);
-    setInterval(function () {
-        canvas2D.clearRect(0, 0, canvas.width, canvas.height);
-        background.draw(canvas2D);
-    }, 30);
-};
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,58 @@
+window.onload = () => {
+
+    var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
+    var canvas2D = canvas.getContext("2d");
+
+    var background = new DisplayObjectContainer();
+
+    var container = new DisplayObjectContainer();
+    container.x = 10;
+    container.y = 20;
+    container.alpha = 0.5;
+    
+    var text1 = new TextField();
+    text1.x = 0;
+    text1.y = 0;
+    text1.alpha = 0.8;
+    text1.color = "#FF0000";
+    text1.fontSize = 30;
+    text1.font = "Arial";
+    text1.text = "I lose my game of life!"
+
+    var text2 = new TextField();
+    text2.x = 0;
+    text2.y = 1;
+    text2.alpha = 1;
+    text2.color = "#0000FF";
+    text2.fontSize = 30;
+    text2.font = "Arial";
+    text2.text = "落命....."
+    
+
+    var bitmap = new Bitmap();
+    bitmap.x = 0;
+    bitmap.y = 0;
+    bitmap.alpha = 0.8;
+    bitmap.scaleX = 1;
+    bitmap.scaleY = 1;
+    bitmap.src = "codmw.png";
+
+    background.addChild(container);
+    container.addChild(text2);
+    background.addChild(text1);
+    background.addChild(bitmap);
+
+    background.draw(canvas2D);
+
+    setInterval(() => {
+        canvas2D.clearRect(0, 0, canvas.width, canvas.height);
+        background.draw(canvas2D);
+    }, 30)
+};
+
 interface Drawable {
+    
     draw(canvas: CanvasRenderingContext2D);
-}
-
-class DisplayObjectContainer extends DisplayObject {
-
-    children: DisplayObject[] = new Array();
-
-    constructor() {
-        super();
-    }
-
-    render(canvas: CanvasRenderingContext2D) {
-        for (var child of this.children) {
-            child.draw(canvas);
-        }
-    }
-
-    addChild(child: DisplayObject) {
-        if (this.children.indexOf(child) == -1) {
-            this.children.push(child);
-            child.parent = this;
-        }
-    }
-
-    removeChild(child: DisplayObject) {
-
-        var tempChildren = this.children.concat();
-
-        for (var element of tempChildren) {
-            if (element == child) {
-                var index = this.children.indexOf(element);
-                tempChildren.splice(index, 1);
-                this.children = tempChildren;
-                return;
-            }
-        }
-    }
 }
 
 class DisplayObject implements Drawable {
@@ -74,17 +91,57 @@ class DisplayObject implements Drawable {
 
         canvas.globalAlpha = this.globalAlpha;
 
+        canvas.setTransform(1,0,0,1,0,0);
+
         canvas.setTransform(this.globalMatrix.a,
             this.globalMatrix.b,
             this.globalMatrix.c,
             this.globalMatrix.d,
             this.globalMatrix.tx,
-            this.globalMatrix.ty)
+            this.globalMatrix.ty);
+        
         this.render(canvas);
     }
 
     //子类重载渲染
     render(canvas: CanvasRenderingContext2D) { }
+}
+
+
+class DisplayObjectContainer extends DisplayObject {
+
+    children: DisplayObject[] = new Array();
+
+    constructor() {
+        super();
+    }
+
+    render(canvas: CanvasRenderingContext2D) {
+        for (var child of this.children) {
+            child.draw(canvas);
+        }
+    }
+
+    addChild(child: DisplayObject) {
+        if (this.children.indexOf(child) == -1) {
+            this.children.push(child);
+            child.parent = this;
+        }
+    }
+
+    removeChild(child: DisplayObject) {
+
+        var tempChildren = this.children.concat();
+
+        for (var element of tempChildren) {
+            if (element == child) {
+                var index = this.children.indexOf(element);
+                tempChildren.splice(index, 1);
+                this.children = tempChildren;
+                return;
+            }
+        }
+    }
 }
 
 class Bitmap extends DisplayObject {
@@ -137,54 +194,3 @@ class TextField extends DisplayObject {
     }
 }
 
-window.onload = () => {
-
-    var canvas = document.getElementById("myCanvas") as HTMLCanvasElement;
-    var canvas2D = canvas.getContext("2d");
-
-    var background = new DisplayObjectContainer();
-
-    var container = new DisplayObjectContainer();
-    container.x = 100;
-    container.y = 50;
-    container.alpha = 0.5;
-    
-    var text1 = new TextField();
-    text1.x = 0;
-    text1.y = 0;
-    text1.alpha = 0.8;
-    text1.color = "#FF0000";
-    text1.fontSize = 30;
-    text1.font = "Arial";
-    text1.text = "I lose my game of life!"
-
-    var text2 = new TextField();
-    text2.x = 100;
-    text2.y = 100;
-    text2.alpha = 1;
-    text2.color = "#0000FF";
-    text2.fontSize = 30;
-    text2.font = "Arial";
-    text2.text = "落命....."
-    
-
-    var bitmap = new Bitmap();
-    bitmap.x = 0;
-    bitmap.y = 0;
-    bitmap.alpha = 0.8;
-    bitmap.scaleX = 1;
-    bitmap.scaleY = 1;
-    bitmap.src = "codmw.png";
-
-    background.addChild(container);
-    container.addChild(text2);
-    background.addChild(text2);
-    background.addChild(bitmap);
-
-    //background.draw(canvas2D);
-
-    setInterval(() => {
-        canvas2D.clearRect(0, 0, canvas.width, canvas.height);
-        background.draw(canvas2D);
-    }, 30)
-};
